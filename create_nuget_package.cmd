cf1f2a48b9d7b935926b04ade7fe1389ed7f858a
untrots <untrots@gmail.com>
2015-11-03 14:59:55 +0200

    Updating libraries

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -19,4 +19,11 @@ xcopy bin\net45\Release\Epicycle.Photogrammetry_cs.dll NuGetPackage\Epicycle.Pho
 xcopy bin\net45\Release\Epicycle.Photogrammetry_cs.pdb NuGetPackage\Epicycle.Photogrammetry-cs.0.1.1.0\lib\net45\
 xcopy bin\net45\Release\Epicycle.Photogrammetry_cs.xml NuGetPackage\Epicycle.Photogrammetry-cs.0.1.1.0\lib\net45\
 
+cd NuGetPackage
+nuget pack Epicycle.Photogrammetry-cs.0.1.1.0\Epicycle.Photogrammetry-cs.0.1.1.0.nuspec -Properties version=0.1.1.0
+7z a -tzip Epicycle.Photogrammetry-cs.0.1.1.0.zip Epicycle.Photogrammetry-cs.0.1.1.0 Epicycle.Photogrammetry-cs.0.1.1.0.nupkg
+
+echo nuget push Epicycle.Photogrammetry-cs.0.1.1.0.nupkg > push.cmd
+echo pause >> push.cmd
+
 pause
